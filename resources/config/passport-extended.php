c6fb9e6f6579b82cc2d205fe78661ad3586193f0
Muhammad Resna Rizki Pratama <resnarizki29@gmail.com>
2018-03-12 22:02:33 +0700

    Update config comment

--- resources/config/passport-extended.php
+++ resources/config/passport-extended.php
@@ -6,10 +6,6 @@ return [
     |--------------------------------------------------------------------------
     | Client Configuration
     |--------------------------------------------------------------------------
-    |
-    | User claims will be loaded from the properties of the auth providers model
-    | specified in the auth config file.
-    |
     */
     'client' => [
         'key_hashid_connection' => 'main',
