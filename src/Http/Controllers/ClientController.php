8e6a8b710fa322b75d4f7f0b303154c4b95c0389
Muhammad Resna Rizki Pratama <resnarizki29@gmail.com>
2018-03-12 13:06:09 +0700

    Override constructor

--- src/Http/Controllers/ClientController.php
+++ src/Http/Controllers/ClientController.php
@@ -3,5 +3,21 @@
 namespace Qwildz\PassportExtended\Http\Controllers;
 
 use Laravel\Passport\Http\Controllers\ClientController as PassportClientController;
+use Illuminate\Contracts\Validation\Factory as ValidationFactory;
+use Qwildz\PassportExtended\ClientRepository;
 
-class ClientController extends PassportClientController{}
+class ClientController extends PassportClientController
+{
+
+    /**
+     * ClientController constructor.
+     * @param ClientRepository $clients
+     * @param ValidationFactory $validation
+     */
+    public function __construct(ClientRepository $clients,
+                                ValidationFactory $validation)
+    {
+        parent::__construct($clients, $validation);
+    }
+
+}
