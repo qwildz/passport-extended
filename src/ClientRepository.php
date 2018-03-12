92fbe5dd3f23ab5fa9bbb3b50d853f8301debda1
Muhammad Resna Rizki Pratama <resnarizki29@gmail.com>
2018-03-12 13:25:16 +0700

    Override some methods

--- src/ClientRepository.php
+++ src/ClientRepository.php
@@ -6,6 +6,32 @@ use Laravel\Passport\ClientRepository as PassportClientRepository;
 
 class ClientRepository extends PassportClientRepository
 {
+    /**
+     * @inheritdoc
+     */
+    public function find($id)
+    {
+        return Client::find($id);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function findForUser($clientId, $userId)
+    {
+        return Client::where('id', $clientId)
+            ->where('user_id', $userId)
+            ->first();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function forUser($userId)
+    {
+        return Client::where('user_id', $userId)
+            ->orderBy('name', 'asc')->get();
+    }
 
     /**
      * Store a new client.
@@ -34,6 +60,30 @@ class ClientRepository extends PassportClientRepository
         ]);
 
         $client->save();
+
+        return $client;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function update(Client $client, $name, $redirect, $trusted, $sso)
+    {
+        $client->forceFill([
+            'name' => $name, 'redirect' => $redirect, 'trusted' => $trusted, 'sso' =>$sso,
+        ])->save();
+
+        return $client;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function regenerateSecret(Client $client)
+    {
+        $client->forceFill([
+            'secret' => str_random(config('passport-extended.client.secret_length', 40)),
+        ])->save();
 
         return $client;
     }
